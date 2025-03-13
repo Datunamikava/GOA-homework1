0b621e923e51a61b34aa74e9d68ffe177c0f715f
datuna <datunamikava000@gmail.com>
2025-03-13 13:16:53 +0400

    commit

---
+++ level 47/ss.js
@@ -0,0 +1,11 @@
+
+let firstName = prompt("შეიყვანეთ სახელი:");
+let lastName = prompt("შეიყვანეთ გვარი:");
+let age = prompt("შეიყვანეთ ასაკი:");
+let location = prompt("შეიყვანეთ საცხოვრებელი ადგილი:");
+let momName=prompt("შეიყვანეთ დედის სახელი:");
+let momlastname=prompt("შეიყვანეთ დედის გვარი:");
+alert ( firstName +  lastName + " არის " + age + " წლის და ცხოვრობს " + location + "მისი დედის სახელია"+ momName + "და მისი გვარია" + momlastname);
+
+
+console.log(sentence);
