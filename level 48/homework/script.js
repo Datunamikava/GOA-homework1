e26e3447da7b967e5d40bf84a52b59c2801c189c
datuna <datunamikava000@gmail.com>
2025-03-28 22:37:53 +0400

    commit

---
+++ level 48/homework/script.js
@@ -0,0 +1,22 @@
+
+let color  = prompt("enter color:")
+
+if (color="red") {
+    console.log(1);
+} else if (color="green"){
+    console.log(2);
+}else if (color="black") {
+    console.log(3);
+}else{
+    console.log("unknow");
+}
+
+
+
+
+
+
+
+red=box1
+green=box2
+black=box3
